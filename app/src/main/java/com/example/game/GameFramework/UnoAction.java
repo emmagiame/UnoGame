d46596164c3d0df515041973ca1bfa6321d892f8
rhian <mckinler26@up.edu>
2023-11-01 15:49:23 -0700

    getter classes

---
+++ app/src/main/java/com/example/game/GameFramework/UnoAction.java
@@ -0,0 +1,52 @@
+package com.example.game.GameFramework;
+
+import com.example.unogame.UnoGameState;
+
+import java.util.ArrayList;
+
+public class UnoAction {
+
+    UnoGameState gameStateRef;
+
+    boolean isValid(ArrayList card) {
+        if(card == gameStateRef.)
+
+
+        ArrayList drawCard(int player) {
+            if(isTurn(player) == true) {
+                //   * onClick() will remove card from draw array
+                //    and add card to player array
+                //    }
+            }
+
+            //   ArrayList playCard(UnoCard card, int player) {
+            //   if(isTurn(player) == true) {
+
+                /*
+                    * onClick() on card will highlight card
+                    * onClick() on discard
+                    * isValid()
+                    * if true {
+                    * onClick() will remove card from player array and insert
+                    into discard array
+    }
+            }
+        }
+
+
+
+    /* declareUno()
+
+    * onClick() will prevent AI from callout()
+
+     */
+
+
+            /* callout()
+
+             * onClick() will add 2 cards to AI array
+
+             */
+        }
+    }
+}
--- app/src/main/java/com/example/unogame/UnoGameState.java
+++ app/src/main/java/com/example/unogame/UnoGameState.java
@@ -43,13 +43,13 @@ public class UnoGameState extends GameState {
      * toString
      *
      * @return type: String
-     *
+     * <p>
      * the method returns a formatted string representation of the game state, including the number of
      * setup turns,the current setup turn, player hands, discard pile, draw pile, and player's turn.
-     * */
+     */
 
     @Override
-    public String toString(){
+    public String toString() {
         return numSetupTurns + SEPARATOR + currentSetupTurn + "player0Hand: " + player0Hand + "player1Hand: " + player1Hand +
                 "Player2Hand: " + player2Hand + "discardPile: " + discardPile + "drawPile: " + drawPile + "playerTurn" + playerTurn;
     }
@@ -77,7 +77,7 @@ public class UnoGameState extends GameState {
         //deep copy of list of players
         this.players = new ArrayList<>();
         for (int player : originalGame.players) {
-            if (player == specificPlayer){
+            if (player == specificPlayer) {
                 //copy only the specific player data
                 this.players.add(specificPlayer);
             } else {
@@ -95,6 +95,33 @@ public class UnoGameState extends GameState {
             cards.add(new UnoCard());
         }
         return cards;
+    }
+    // creates inital draw pile of 87 (max cards used by players initally is 21 bc 7*3
+    // and max num of cards is 108 so draw = 108-21) cards for start of game
+    ArrayList startDrawPile() {
+        ArrayList<UnoCard> cards = new ArrayList<UnoCard>();
+
+        for(int i = 0; i <= 87; i++) {
+            cards.add(new UnoCard());
+        }
+        return cards;
+    }
+
+    // getter methods
+    public ArrayList<UnoCard> getPlayer0Hand() {
+        return player0Hand;
+    }
+
+    public ArrayList<UnoCard> getPlayer1Hand() {
+        return player1Hand;
+    }
+
+    public ArrayList<UnoCard> getDrawPile() {
+        return drawPile;
+    }
+
+    public ArrayList<UnoCard> getDiscardPile() {
+        return discardPile;
     }
 }
 
