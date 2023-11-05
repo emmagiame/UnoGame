246f853506f39057dec47e65c9f9305b2c87f60a
giamello26 <giamello26@up.edu>
2023-11-05 10:43:22 -0800

    added more informative comments

--- app/src/main/java/com/example/unogame/UnoAction.java
+++ app/src/main/java/com/example/unogame/UnoAction.java
@@ -27,15 +27,27 @@ public class UnoAction extends GameAction {
      *      returns true if card can be played, false otherwise
      */
     public boolean isValid(ArrayList card) {
-        if(card == gameStateRef.)
+        /*
+        if (card == gameStateRef.) {
 
+        }
+        */
+        return false;
+    }
 
-        ArrayList drawCard(int player) {
-            if(isTurn(player) == true) {
+    /**
+     * draws a card for a given player
+     *
+     * @param player - player drawing card
+     * @return
+     *      new hand with drawn card
+     */
+    public ArrayList drawCard(int player) {
+        if(isTurn(player) == true) {
                 //   * onClick() will remove card from draw array
                 //    and add card to player array
                 //    }
-            }
+        }
 
             //   ArrayList playCard(UnoCard card, int player) {
             //   if(isTurn(player) == true) {
@@ -47,24 +59,23 @@ public class UnoAction extends GameAction {
                     * if true {
                     * onClick() will remove card from player array and insert
                     into discard array
+                */
+
+        return null;
     }
-            }
-        }
 
 
+    /*
+    public boolean declareUno() {
 
-    /* declareUno()
+    // onClick() will prevent AI from callOut()
 
-    * onClick() will prevent AI from callout()
-
-     */
-
-
-            /* callout()
-
-             * onClick() will add 2 cards to AI array
-
-             */
-        }
     }
+
+
+    public ArrayList callOut(){
+        // onClick() will add 2 cards to AI array
+
+    }
+    */
 }
