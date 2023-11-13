3aa1458464461d080abee4f41c4e88111720e182
horstman26 <horstman26@up.edu>
2023-11-13 14:59:35 -0800

    created UnoDumbAIPlayer and added comments

---
+++ app/src/main/java/com/example/unogame/UnoDumbAIPlayer.java
@@ -0,0 +1,9 @@
+package com.example.unogame;
+
+public class UnoDumbAIPlayer {
+    //if it has a playable card, play the first playable card it has
+
+    //if it doesnt have a playable card, draw a card
+
+    //if one card left, %50 chance to call uno, else do nothing - need to write a call uno function (in UnoGameState)
+}
