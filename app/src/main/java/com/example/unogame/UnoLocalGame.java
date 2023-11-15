925878f5efdb307b6d3f51acc63c96a1f4c58566
rhian <mckinler26@up.edu>
2023-11-14 18:32:07 -0800

    added "make move" method

--- app/src/main/java/com/example/unogame/UnoLocalGame.java
+++ app/src/main/java/com/example/unogame/UnoLocalGame.java
@@ -7,9 +7,11 @@ import com.example.unogame.info.UnoGameState;
 import com.example.unogame.players.UnoHumanPlayer;
 
 public class UnoLocalGame extends LocalGame {
+
+    UnoGameState unoGameStateRef;
     //needs to be like ttt, the state will have a variable keeping track of the player whose turn it it and number of players
-        //(x + 2) % 3 will skip a turn
-        //state.setwhosemove() = line above this
+    //(x + 2) % 3 will skip a turn
+    //state.setwhosemove() = line above this
     //
 
     private UnoGameState refOfficialGame;
@@ -17,13 +19,12 @@ public class UnoLocalGame extends LocalGame {
     /**
      * constructor
      */
-    public UnoLocalGame(){ //this wont work past alpha
+    public UnoLocalGame() { //this wont work past alpha
         refOfficialGame = new UnoGameState(2);
     }
 
     @Override
-    public void start(GamePlayer[] players)
-    {
+    public void start(GamePlayer[] players) {
         super.start(players);
         state = new UnoGameState(getNumPlayers());
     }
@@ -33,12 +34,10 @@ public class UnoLocalGame extends LocalGame {
      * check if it is given players turn
      */
     protected boolean canMove(int player) {
-        UnoGameState gameStateRef = (UnoGameState)state;
-        if(player == gameStateRef.getCurrentSetupTurn()) {
+        UnoGameState gameStateRef = (UnoGameState) state;
+        if (player == gameStateRef.getCurrentSetupTurn()) {
             return true;
-        }
-
-        else {
+        } else {
             return false;
         }
     }
@@ -46,42 +45,131 @@ public class UnoLocalGame extends LocalGame {
     /**
      * Makes move dependant on the action passed through
      *
-     * @param action
-     * 			The move that the player has sent to the game
-     * @return
-     *      true if action was taken or false if action was invalid/illegal
+     * @param action The move that the player has sent to the game
+     * @return true if action was taken or false if action was invalid/illegal
      */
     @Override
     protected boolean makeMove(GameAction action) {
-        if(action instanceof UnoPlayCardAction){
-            //look at pig
+
+        // if action is play card
+        if (action instanceof UnoPlayCardAction) {
+            // check if it's that players turn
+            if (canMove(getPlayerIdx(action.getPlayer())) == true) {
+                // get num players
+                int numPlayers = getNumPlayers();
+
+                // get player id
+                int id = unoGameStateRef.getPlayerTurn();
+                // play card DOES NOT WORK BC IDK HOW TO GET
+                // THE CARD
+                // unoGameStateRef.playCard(id, card);
+                // change the current player to the next player
+
+                // if two players
+                if (numPlayers == 2) {
+                    // if id 0 turn
+                    if (id == 0) {
+                        unoGameStateRef.setPlayerTurn(1);
+                    }
+                    // if id 1 turn
+                    else if (id == 1) {
+                        unoGameStateRef.setPlayerTurn(0);
+                    }
+
+                }
+
+                // if three players
+                if (numPlayers == 3) {
+                    // if id 0 turn
+                    if (id == 0) {
+                        unoGameStateRef.setPlayerTurn(1);
+                    }
+                    // if id 1 turn
+                    else if (id == 1) {
+                        unoGameStateRef.setPlayerTurn(2);
+                    }
+
+                    // if id 2 turn
+                    else if (id == 2) {
+                        unoGameStateRef.setPlayerTurn(0);
+                    }
+
+                }
+            }
         }
+
+        // if action is draw card
+        if (action instanceof UnoDrawCardAction) {
+            // check if it's that players turn
+            if (canMove(getPlayerIdx(action.getPlayer())) == true) {
+                // get num players
+                int numPlayers = getNumPlayers();
+
+                // get player id
+                int id = unoGameStateRef.getPlayerTurn();
+                // draw card
+                // unoGameStateRef.drawCardFromDrawPile(id,card);
+
+                // change the current player to the next player
+
+                // if two players
+                if (numPlayers == 2) {
+                    // if id 0 turn
+                    if (id == 0) {
+                        unoGameStateRef.setPlayerTurn(1);
+                    }
+                    // if id 1 turn
+                    else if (id == 1) {
+                        unoGameStateRef.setPlayerTurn(0);
+                    }
+
+                }
+
+                // if three players
+                if (numPlayers == 3) {
+                    // if id 0 turn
+                    if (id == 0) {
+                        unoGameStateRef.setPlayerTurn(1);
+                    }
+                    // if id 1 turn
+                    else if (id == 1) {
+                        unoGameStateRef.setPlayerTurn(2);
+                    }
+
+                    // if id 2 turn
+                    else if (id == 2) {
+                        unoGameStateRef.setPlayerTurn(0);
+                    }
+
+                }
+            }
+        }
+
+        /**
+         * send updated state to a given player
+         *
+         * @param p
+         * 			the player to notify
+         */
+        @Override
+        protected void sendUpdatedStateTo (GamePlayer p){
+
+        }
+
+        /**
+         * check if game is over
+         *
+         * @return
+         *      a message that tells who has won the game,
+         *      or null if the game is not over
+         */
+        @Override
+        protected String checkIfGameOver () {
+            return null;
+        }
+
+        // check if passed in card has at least one
+        // element of card at beginning of discard arraylist
+
     }
-
-    /**
-     * send updated state to a given player
-     *
-     * @param p
-     * 			the player to notify
-     */
-    @Override
-    protected void sendUpdatedStateTo(GamePlayer p) {
-
-    }
-
-    /**
-     * check if game is over
-     *
-     * @return
-     *      a message that tells who has won the game,
-     *      or null if the game is not over
-     */
-    @Override
-    protected String checkIfGameOver() {
-        return null;
-    }
-
-    // check if passed in card has at least one
-         // element of card at beginning of discard arraylist
-
 }
